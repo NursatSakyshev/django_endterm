37be6b9db3a0fe5643560f28416a677a2e8c4e67
_ <orazgalialmas@gmail.com>
2024-11-15 22:18:12 +0600

    add models into admin page

--- endterm/my_app/models.py
+++ endterm/my_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
